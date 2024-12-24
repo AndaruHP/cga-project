4f4213bf6187ec826b67cbd2f860f4616508611e
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2024-12-24 21:59:06 +0700

    main menu

---
+++ Assets/Menu/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        // we set + 1 because in our setting we have two scenes, the first one is the menu and the second one is the game
+        // and its order is 0 then 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        // this only work in the build version of the game
+        Debug.Log("Player has quit the game");
+        Application.Quit();
+    }
+}
